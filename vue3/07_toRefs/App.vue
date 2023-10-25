292967d91e0a7039f647efff7c93cbba94bb3426
leoliyou <liyouleoleo@gmail.com>
2023-10-25 16:18:27 +0100

    vue3

---
+++ vue3/07_toRefs/App.vue
@@ -0,0 +1,28 @@
+<template>
+<h2>name:{{ name }}</h2>
+<h2>age:{{ age }}</h2>
+</template>
+
+<script lang="ts">
+import { reactive,toRefs} from 'vue'
+
+
+export default {
+  setup() {
+    const user = reactive({
+      name: 'xiao',
+      age:16
+    })
+    const { name,age} = toRefs(user)
+    // setInterval(() => {
+    //   name.value += "++"
+    //   age.value++
+    // },1000)
+
+    return {
+    name,age
+    }
+  }
+
+}
+</script>
---
+++ vue3/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { reactive,toRefs} from 'vue'
+
+
+export default {
+  setup() {
+
+
+    return {
+    
+    }
+  }
+
+}
+</script>
